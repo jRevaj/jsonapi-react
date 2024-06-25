0437eb570265391494943507d2398bfd8e118990
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2024-06-25 18:48:45 +0200

    just testing

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-import size from 'rollup-plugin-size'
+// import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+// import size from 'rollup-plugin-size'
 import pkg from './package.json'
 
 export default [
@@ -21,13 +21,13 @@ export default [
       resolve(),
       babel(),
       commonjs(),
-      size({
-        publish: true,
-        exclude: pkg.module,
-        filename: 'sizes-cjs.json',
-        writeFile: false
-      }),
-      sizeSnapshot(),
+      // size({
+      //   publish: true,
+      //   exclude: pkg.module,
+      //   filename: 'sizes-cjs.json',
+      //   writeFile: false
+      // }),
+      // sizeSnapshot(),
     ],
   },
   {
@@ -42,13 +42,13 @@ export default [
         includeDependencies: true,
       }),
       babel(),
-      size({
-        publish: true,
-        exclude: pkg.module,
-        filename: 'sizes-es.json',
-        writeFile: false
-      }),
-      sizeSnapshot(),
+      // size({
+      //   publish: true,
+      //   exclude: pkg.module,
+      //   filename: 'sizes-es.json',
+      //   writeFile: false
+      // }),
+      // sizeSnapshot(),
     ],
   },
 ]
